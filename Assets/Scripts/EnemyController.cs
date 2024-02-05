4957ce7b3ed97d9d08d780c350500d2efae98545
Cayden Andrews <144056074+D761454@users.noreply.github.com>
2024-02-05 15:24:10 +0000

    Enemy Able to Take Damage

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -65,4 +65,12 @@ public class EnemyController : MonoBehaviour
     {
         m_health -= 1;
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Projectile")
+        {
+            TakeDamage();
+        }
+    }
 }
