ca07a72e33e48ff9eac58dc8c0c4052c4a72c7ec
Cayden Andrews <144056074+D761454@users.noreply.github.com>
2024-02-25 15:26:09 +0000

    flash

--- Assets/Scripts/UI/FlashEffect.cs
+++ Assets/Scripts/UI/FlashEffect.cs
@@ -7,43 +7,20 @@ public class FlashEffect : MonoBehaviour
 {
     public Image FlashScreen;
 
-    private Color white => Color.white;
+    private Color white = Color.white;
     private Color none = new Color (0, 0, 0, 0);
-
-    private void Start()
-    {
-        FlashScreen.gameObject.SetActive(false);
-    }
 
     public void Begin()
     {
-        FlashScreen.gameObject.SetActive(true);
-        FlashScreen.color = none;
         StartCoroutine(Lerp(none, white, 5/3, 3));
     }
-
-    public void End()
-    {
-        FlashScreen.gameObject.SetActive(false);
-    }
-
-    // Update is called once per frame
-    //public void Update()
-    //{
-    //    FlashScreen.color = Lerp(none, white, 3.5f);
-    //}
-
-    //public Color Lerp(Color initial, Color end, float lifespan)
-    //{
-    //    return Color.Lerp(initial, end, Mathf.Sin(Time.time * lifespan));
-    //}
 
     IEnumerator Lerp(Color initial, Color end, float lifespan, int times)
     {
         float age = 0.0f;
         float frac = 0.0f;
 
-        do
+        while (times > 0) 
         {
             do
             {
@@ -61,11 +38,11 @@ public class FlashEffect : MonoBehaviour
                 age -= Time.deltaTime;
 
                 FlashScreen.color = Color.Lerp(initial, end, frac);
-                times--;
 
                 yield return null;
             } while (frac > 0.0f);
-        } while (times > 0);
-        
+            times--;
+            yield return null;
+        }
     }
 }
--- Assets/Scripts/WaveSystem.cs
+++ Assets/Scripts/WaveSystem.cs
@@ -25,7 +25,6 @@ public class WaveSystem : MonoBehaviour
         // increment wave and calculate total enemies for wave
         wave++;
         m_zToSpawn = (wave * 2) + 5;
-        m_flash.End();
         m_NWCR = false;
 
         if (wave % 5 == 0)
