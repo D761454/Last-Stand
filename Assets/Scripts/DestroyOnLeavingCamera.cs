b05322ea39d4a772d6640a75b0cec76670c10ef0
Cayden Andrews <144056074+D761454@users.noreply.github.com>
2024-02-25 13:46:36 +0000

    bullet life span and animations

--- Assets/Scripts/DestroyOnLeavingCamera.cs
+++ Assets/Scripts/DestroyOnLeavingCamera.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class DestroyOnLeavingCamera : MonoBehaviour
 {
+    [SerializeField] private float m_lifespan;
+    private float m_fireTime;
+    private void Start()
+    {
+        m_fireTime = Time.time;  
+    }
+
     private void OnBecameInvisible()
     {
         Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        if ((Time.time - m_fireTime) > m_lifespan)
+        {
+            Destroy(gameObject);
+        }
     }
 }
