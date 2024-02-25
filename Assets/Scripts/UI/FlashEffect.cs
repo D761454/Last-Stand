3a2255bf4c44bc0fcbd32f4070fdb4492af30902
Cayden Andrews <144056074+D761454@users.noreply.github.com>
2024-02-25 15:10:10 +0000

    Update FlashEffect.cs

--- Assets/Scripts/UI/FlashEffect.cs
+++ Assets/Scripts/UI/FlashEffect.cs
@@ -17,8 +17,9 @@ public class FlashEffect : MonoBehaviour
 
     public void Begin()
     {
-        FlashScreen.color = none;
         FlashScreen.gameObject.SetActive(true);
+        FlashScreen.color = none;
+        StartCoroutine(Lerp(none, white, 5/3, 3));
     }
 
     public void End()
@@ -27,13 +28,44 @@ public class FlashEffect : MonoBehaviour
     }
 
     // Update is called once per frame
-    public void Update()
-    {
-        FlashScreen.color = Lerp(none, white, 3.5f);
-    }
+    //public void Update()
+    //{
+    //    FlashScreen.color = Lerp(none, white, 3.5f);
+    //}
 
-    public Color Lerp(Color initial, Color end, float time)
+    //public Color Lerp(Color initial, Color end, float lifespan)
+    //{
+    //    return Color.Lerp(initial, end, Mathf.Sin(Time.time * lifespan));
+    //}
+
+    IEnumerator Lerp(Color initial, Color end, float lifespan, int times)
     {
-        return Color.Lerp(initial, end, Mathf.Sin(Time.time * time));
+        float age = 0.0f;
+        float frac = 0.0f;
+
+        do
+        {
+            do
+            {
+                frac = age / lifespan;
+                age += Time.deltaTime;
+
+                FlashScreen.color = Color.Lerp(initial, end, frac);
+
+                yield return null;
+            } while (frac < 1.0f);
+
+            do
+            {
+                frac = age / lifespan;
+                age -= Time.deltaTime;
+
+                FlashScreen.color = Color.Lerp(initial, end, frac);
+                times--;
+
+                yield return null;
+            } while (frac > 0.0f);
+        } while (times > 0);
+        
     }
 }
